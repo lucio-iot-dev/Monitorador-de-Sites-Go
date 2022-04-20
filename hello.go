ac8c5d7c2707be05ae5b78f94a81313e1b775000
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-19 23:17:29 -0300

    segunda aula

--- hello.go
+++ hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World! Lúcio!!")
+    var nome string = "Douglas"
+    var idade int
+    var versao float32 = 1.1
+    fmt.Println("Olá sr.", nome, "sua idade é", idade)
+    fmt.Println("Este programa está na versão", versao)
 }
