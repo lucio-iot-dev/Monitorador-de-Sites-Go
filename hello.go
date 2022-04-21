c3b03aaff5fabb2e864d25ef88b5c58446cb9f83
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-21 00:31:58 -0300

    Aprendendo if/else e switch case

--- hello.go
+++ hello.go
@@ -1,28 +1,47 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
-// func main() {
-//     var nome string = "Douglas"
-//     var idade int = 24
-//     var versao float32 = 1.1
-//     fmt.Println("Olá sr.", nome, "sua idade é", idade)
-//     fmt.Println("Este programa está na versão", versao)
 
-//     fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
-// }
-
-/*-----Declaração curta de variáveis--------------*/
 
 func main() {
-    nome := "Douglas"
-    idade := 24
+    nome := "Lúcio"
     versao := 1.1
-    fmt.Println("Olá sr.", nome, "sua idade é", idade)
+    fmt.Println("Olá sr.", nome)
     fmt.Println("Este programa está na versão", versao)
 
-    fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+    fmt.Println("1- Iniciar Monitoramento")
+    fmt.Println("2- Exibir Logs")
+    fmt.Println("0- Sair do Programa")
+
+    var comando int
+    fmt.Scan(&comando)
+
+    fmt.Println("O valor da variável comando é:", comando)
+
+    if comando == 1 {
+        fmt.Println("Monitorando...")
+    } else if comando == 2 {
+        fmt.Println("Exibindo Logs...")
+    } else if comando == 0 {
+        fmt.Println("Saindo do programa")
+    } else {
+        fmt.Println("Não conheço este comando")
+    }
+/*---------------Outra forma de fazer----------------*/
+
+    switch comando {
+    case 1:
+        fmt.Println("Monitorando...")
+    case 2:
+        fmt.Println("Exibindo Logs...")
+    case 3:
+        fmt.Println("Saindo do programa")
+    default:
+        fmt.Println("Não conheço este comando")
+    }
+    // o switch não precisa utilizar o comando break para parar a execução....ele por si só já vai sair sa função caso alguem escolher um item
+
+
+
 }
