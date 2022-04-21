38cecfe91248f2d0913e497ee94a093455b8b5db
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-21 12:11:31 -0300

    melhoria do codigo

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -10,37 +11,41 @@ import (
 func main() {
 
     exibeIntroducao()
-       
-    exibirMenu()
-    
-    comando := lerComando()
-/*---- no if sempre retornará booleano-----*/
-    // if comando == 1 {
-    //     fmt.Println("Monitorando...")
-    // } else if comando == 2 {
-    //     fmt.Println("Exibindo Logs...")
-    // } else if comando == 0 {
-    //     fmt.Println("Saindo do programa")
-    // } else {
-    //     fmt.Println("Não conheço este comando")
-    // }
-/*---------------Outra forma de fazer----------------*/
 
-    switch comando {
-    case 1:
-        fmt.Println("Monitorando...")
-    case 2:
-        fmt.Println("Exibindo Logs...")
-    case 0:
-        fmt.Println("Saindo do programa")
-        os.Exit(0)
-    default:
-        fmt.Println("Não conheço este comando")
-        os.Exit(-1)
-    }
-    // o switch não precisa utilizar o comando break para parar a execução....ele por si só já vai sair sa função caso alguem escolher um item
-}
+    for {
 
+        
+        exibirMenu()
+        
+        comando := lerComando()
+        /*---- no if sempre retornará booleano-----*/
+        // if comando == 1 {
+            //     fmt.Println("Monitorando...")
+            // } else if comando == 2 {
+                //     fmt.Println("Exibindo Logs...")
+                // } else if comando == 0 {
+                    //     fmt.Println("Saindo do programa")
+                    // } else {
+                        //     fmt.Println("Não conheço este comando")
+                        // }
+                        /*---------------Outra forma de fazer----------------*/
+                        
+                        switch comando {
+                        case 1:
+                            iniciarMonitoramento()
+                        case 2:
+                            fmt.Println("Exibindo Logs...")
+                        case 0:
+                            fmt.Println("Saindo do programa")
+                            os.Exit(0)
+                        default:
+                            fmt.Println("Não conheço este comando")
+                            os.Exit(-1)
+                        }
+                        // o switch não precisa utilizar o comando break para parar a execução....ele por si só já vai sair sa função caso alguem escolher um item
+                    }
+                }
+                    
 func exibeIntroducao() {
     nome := "Lúcio"
     versao := 1.1
@@ -62,4 +67,16 @@ func lerComando() int {
     fmt.Println("O valor da variável comando é:", comandoLido)
 
     return comandoLido
+}
+
+func iniciarMonitoramento() {
+    fmt.Println("Monitorando...")
+    site := "https://www.alura.com.br"
+    resp, _ := http.Get(site)
+    
+    if resp.StatusCode == 200 {
+        fmt.Println("Site:", site, "foi carregado com sucesso!")
+    } else {
+        fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+    }
 }
