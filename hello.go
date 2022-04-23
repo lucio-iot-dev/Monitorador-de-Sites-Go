f9f5da5beb64dfca3406b612f443c3f0d8399c32
Lúcio do Nascimento <luciomate95@gmail.com>
2022-04-22 22:28:11 -0300

    implementaçao de slices

--- hello.go
+++ hello.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
-
+const monitoramentos = 2
+const delay = 5
 
 func main() {
 
@@ -64,19 +66,43 @@ func lerComando() int {
     var comandoLido int
     fmt.Scan(&comandoLido)
 
-    fmt.Println("O valor da variável comando é:", comandoLido)
+    fmt.Println("O comando escolhido foi:", comandoLido)
+    fmt.Println("")
+
 
     return comandoLido
 }
 
 func iniciarMonitoramento() {
     fmt.Println("Monitorando...")
-    site := "https://www.alura.com.br"
-    resp, _ := http.Get(site)
+    sites := []string {"https://random-status-code.herokuapp.com/", "https://www.alura.com.br", "https://www.caelum.com.br"}
+
+    // esse metodo pode ser subtituido em go por range
+    // for i := 0; i < len(sites); i ++ {
+    // fmt.Println(sites[i])
+    // }
+
+    for i := 0; i < monitoramentos; i++ {
+
+        
+        for i, site := range sites {
+            fmt.Println("Testando site", i, ":", site)
+            testaSite(site)
+        }
+        time.Sleep(delay * time.Second)
+        fmt.Println("")
+    }
+
+    fmt.Println("")
+
+}
+
+     func testaSite(site string) {
+        resp, _ := http.Get(site)
     
     if resp.StatusCode == 200 {
         fmt.Println("Site:", site, "foi carregado com sucesso!")
     } else {
         fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
     }
-}
+    }
