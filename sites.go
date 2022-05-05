4b22cae8eefdafbce7d676e74b4d281c60c8f81e
Lúcio do Nascimento <luciomate95@gmail.com>
2022-05-04 23:27:28 -0300

    criando arquivo log.txt e imprimindo no terminal

--- sites.go
+++ sites.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"strconv"
 	"strings"
 
 	// "io/ioutil"
@@ -18,7 +20,7 @@ const delay = 1
 func main() {
 
 	exibeIntroducao()
-	leSitesDoArquivo()
+	
 
 	for {
 
@@ -42,6 +44,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -111,8 +114,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -152,4 +157,26 @@ func leSitesDoArquivo() []string {
     arquivo.Close()
        
     return sites
+}
+
+func registraLog(site string, status bool) {
+  arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+  
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + "-" + site + " - online: " + strconv.FormatBool(status) + "\n") // \n pular linha
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+  arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+	fmt.Println(err)
+
+	}
+	fmt.Println(string(arquivo))
 }
